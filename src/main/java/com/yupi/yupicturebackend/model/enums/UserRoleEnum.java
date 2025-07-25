bd9f40cb4044c84e8f1591101b48abb635ac6eda
guoziyangnb <2263831821@qq.com>
2025-07-25 19:00:25 +0800

    feat: Map优化枚举类

--- src/main/java/com/yupi/yupicturebackend/model/enums/UserRoleEnum.java
+++ src/main/java/com/yupi/yupicturebackend/model/enums/UserRoleEnum.java
@@ -3,6 +3,9 @@ package com.yupi.yupicturebackend.model.enums;
 import cn.hutool.core.util.ObjUtil;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 用户角色枚举
  */
@@ -21,6 +24,18 @@ public enum UserRoleEnum {
     }
 
     /**
+     * 利用Map优化枚举类，避免获取一次枚举值就遍历一次枚举对象
+     * 1.首先创造一个枚举类，然后依次填入
+     */
+    private static final Map<String ,UserRoleEnum> VALUE_MAP = new HashMap<>();
+
+    static {
+        for (UserRoleEnum roleEnum : UserRoleEnum.values()){
+            VALUE_MAP.put(roleEnum.value, roleEnum);
+        }
+    }
+
+    /**
      * 根据 value 获取枚举
      *
      * @param value 枚举值的value
@@ -30,11 +45,16 @@ public enum UserRoleEnum {
         if (ObjUtil.isEmpty(value)) {
             return null;
         }
-        for (UserRoleEnum anEnum : UserRoleEnum.values()) {
-            if (anEnum.value.equals(value)) {
-                return anEnum;
-            }
-        }
-        return null;
+//        for (UserRoleEnum anEnum : UserRoleEnum.values()) {
+//            if (anEnum.value.equals(value)) {
+//                return anEnum;
+//            }
+//        }
+//        return null;
+        /**
+         * 优化
+         * 2.之后返回枚举值
+         */
+        return VALUE_MAP.get(value);
     }
 }
