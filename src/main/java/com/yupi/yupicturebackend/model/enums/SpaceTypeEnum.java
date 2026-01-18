248db3707921d35c0a5fe5065629f43b344123b9
gzynb <2263831821@qq.com>
2026-01-19 00:21:58 +0800

    feat: 增加空间类型枚举

---
+++ src/main/java/com/yupi/yupicturebackend/model/enums/SpaceTypeEnum.java
@@ -0,0 +1,44 @@
+package com.yupi.yupicturebackend.model.enums;
+
+import cn.hutool.core.util.ObjUtil;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 空间类型枚举
+ */
+@Getter
+public enum SpaceTypeEnum {
+
+    PRIVATE("私有空间", 0),
+    TEAM("团队空间", 1);
+
+    private final String text;
+
+    private final int value;
+
+    SpaceTypeEnum(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    private static final Map<Integer ,SpaceTypeEnum> VALUE_MAP = new HashMap<>();
+
+    static {
+        for (SpaceTypeEnum spaceTypeEnum : SpaceTypeEnum.values()){
+            VALUE_MAP.put(spaceTypeEnum.value, spaceTypeEnum);
+        }
+    }
+
+    /**
+     * 根据 value 获取枚举
+     */
+    public static SpaceTypeEnum getEnumByValue(Integer value) {
+        if (ObjUtil.isEmpty(value)) {
+            return null;
+        }
+        return VALUE_MAP.get(value);
+    }
+}
