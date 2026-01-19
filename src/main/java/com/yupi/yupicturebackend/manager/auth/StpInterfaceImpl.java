fdb0cf4f8f49c5294b71703d6fdd856f8ecbb591
gzynb <2263831821@qq.com>
2026-01-19 14:46:11 +0800

    feat:sa-Token编程式获取上下文

---
+++ src/main/java/com/yupi/yupicturebackend/manager/auth/StpInterfaceImpl.java
@@ -0,0 +1,86 @@
+package com.yupi.yupicturebackend.manager.auth;
+
+import cn.dev33.satoken.stp.StpInterface;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.http.ContentType;
+import cn.hutool.http.Header;
+import cn.hutool.json.JSONUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * 自定义权限加载接口实现类
+ */
+@Component    // 保证此类被 SpringBoot 扫描，完成 Sa-Token 的自定义权限验证扩展
+public class StpInterfaceImpl implements StpInterface {
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    /**
+     * 返回一个账号所拥有的权限码集合 
+     */
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        new ArrayList<>();
+    }
+
+    /**
+     * 本项目用不到，返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
+     */
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        new ArrayList<>();
+    }
+
+    /**
+     * 从请求中获取上下文对象
+     */
+    private SpaceUserAuthContext getAuthContextByRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String contentType = request.getHeader(Header.CONTENT_TYPE.getValue());
+        SpaceUserAuthContext authRequest;
+        // 获取请求参数，兼容 get 和 post 操作
+        if (ContentType.JSON.getValue().equals(contentType)) { // ppost请求
+            String body = ServletUtil.getBody(request);
+            authRequest = JSONUtil.toBean(body, SpaceUserAuthContext.class);
+        } else {
+            // get请求
+            Map<String, String> paramMap = ServletUtil.getParamMap(request);
+            authRequest = BeanUtil.toBean(paramMap, SpaceUserAuthContext.class);
+        }
+        // 根据请求路径区分 id 字段的含义
+        Long id = authRequest.getId();
+        if (ObjUtil.isNotNull(id)) {
+            // 获取到请求路径的业务前缀，/api/picture/a?aaa=1
+            String requestUri = request.getRequestURI();
+            // 替换上下文，剩下的就是前缀，picture/a?aaa=1
+            String partUri = requestUri.replace(contextPath + "/", "");
+            // 获取前缀的第一个斜杠前的字符串
+            String moduleName = StrUtil.subBefore(partUri, "/", false);
+            switch (moduleName) {
+                case "picture":
+                    authRequest.setPictureId(id);
+                    break;
+                case "spaceUser":
+                    authRequest.setSpaceUserId(id);
+                    break;
+                case "space":
+                    authRequest.setSpaceId(id);
+                    break;
+                default:
+            }
+        }
+        return authRequest;
+    }
+}
